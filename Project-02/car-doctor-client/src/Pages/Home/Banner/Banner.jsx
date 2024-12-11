d6dedb0027292bf4daeb34a6ea850bbc642565ab
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-12-12 03:30:06 +0600

    avoiding repeatation

--- Project-02/car-doctor-client/src/Pages/Home/Banner/Banner.jsx
+++ Project-02/car-doctor-client/src/Pages/Home/Banner/Banner.jsx
@@ -4,93 +4,35 @@ import img3 from '../../../assets/images/banner/3.jpg';
 import img4 from '../../../assets/images/banner/4.jpg';
 import img5 from '../../../assets/images/banner/5.jpg';
 import img6 from '../../../assets/images/banner/6.jpg';
+
+const images = [img1, img2, img3, img4, img5, img6];
+
 const Banner = () => {
   return (
     <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src={img1}
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
+      {images.map((img, index) => (
+        <div
+          key={index}
+          id={`slide${index + 1}`}
+          className="carousel-item relative w-full"
+        >
+          <img src={img} className="w-full" alt={`Slide ${index + 1}`} />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a
+              href={`#slide${(index === 0 ? images.length : index)}`}
+              className="btn btn-circle"
+            >
+              ❮
+            </a>
+            <a
+              href={`#slide${(index + 2 > images.length ? 1 : index + 2)}`}
+              className="btn btn-circle"
+            >
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src={img2}
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src={img3}
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src={img4}
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide5" className="carousel-item relative w-full">
-        <img
-          src={img5}
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide6" className="carousel-item relative w-full">
-        <img
-          src={img6}
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
